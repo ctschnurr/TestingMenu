e3ba5f120d6694b7a3d2b0cf534463c0b656bc33
ctschnurr <113189402+ctschnurr@users.noreply.github.com>
2022-10-31 11:48:27 -0300

    Initial commit

---
+++ TestingMenu/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+
+        static bool gameOver;
+
+        static int newChoice;
+        static string playerChose;
+
+        static int maxX = 60;
+        static int maxY = 20;
+        static void Main(string[] args)
+        {
+            gameOver = false;
+            string[] options = { "Main", "Forest", "Castle" };
+
+            newChoice = Choice(true, options);
+            playerChose = options[newChoice];
+            Console.Clear();
+
+            Console.WriteLine("");
+            Console.WriteLine("Player chose " + playerChose);
+            Console.ReadKey(true);
+
+            while (gameOver == false)
+            {
+
+            }
+        }
+        public static int Choice(bool canCancel, params string[] options)
+        {
+            const int startX = 1;
+            const int startY = 1;
+            const int optionsPerLine = 1;
+            const int spacingPerLine = 14;
+
+            int currentSelection = 0;
+
+            ConsoleKey key;
+
+            Console.CursorVisible = false;
+
+            do
+            {
+                Console.Clear();
+
+                for (int i = 0; i < options.Length; i++)
+                {
+                    Console.SetCursorPosition(startX + (i % optionsPerLine) * spacingPerLine, startY + i / optionsPerLine);
+
+                    if (i == currentSelection)
+                        Console.BackgroundColor = ConsoleColor.Blue;
+
+                    Console.Write(options[i]);
+
+                    Console.ResetColor();
+                }
+
+                key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        {
+                            if (currentSelection % optionsPerLine > 0)
+                                currentSelection--;
+                            break;
+                        }
+                    case ConsoleKey.RightArrow:
+                        {
+                            if (currentSelection % optionsPerLine < optionsPerLine - 1)
+                                currentSelection++;
+                            break;
+                        }
+                    case ConsoleKey.UpArrow:
+                        {
+                            if (currentSelection >= optionsPerLine)
+                                currentSelection -= optionsPerLine;
+                            break;
+                        }
+                    case ConsoleKey.DownArrow:
+                        {
+                            if (currentSelection + optionsPerLine < options.Length)
+                                currentSelection += optionsPerLine;
+                            break;
+                        }
+                    case ConsoleKey.Escape:
+                        {
+                            if (canCancel)
+                                return -1;
+                            break;
+                        }
+                }
+            } while (key != ConsoleKey.Enter);
+
+            Console.CursorVisible = true;
+
+            return currentSelection;
+        }
+    }
+}
